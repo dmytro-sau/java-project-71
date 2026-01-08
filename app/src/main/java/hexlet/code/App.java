d0648bac77f8487224e16a2f6de0ddfc6e4f28c1
Dmytro Sau <dmytro.sau@proton.me>
2026-01-08 23:19:24 +0100

    Add picocli command definition

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,16 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(
+        name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,7 @@
+package hexlet.code;
+
+public class Differ {
+    public static void generate(String path1, String path2) {
+
+    }
+}
